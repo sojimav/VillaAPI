f377c2a8aacf2224376f9574dd91ad90ff00217c
Victor Ajibade <adesojimav@gmail.com>
2023-07-21 04:22:29 +0100

    updates

--- MagicVillaAPI/Controllers/VillaAPIController.cs
+++ MagicVillaAPI/Controllers/VillaAPIController.cs
@@ -6,7 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVillaAPI.Controllers
 {
-	[Route("api/[controller]")]  // it is always better to use in specific name instead of the [controller] syntax
+	// it is always better to use in specific name instead of the [controller] syntax
+	[Route("api/[controller]")]  
 	[ApiController]
 	public class VillaAPIController : ControllerBase
 	{
@@ -16,12 +17,16 @@ namespace MagicVillaAPI.Controllers
 			return Ok(VillaStore.villaDtos);
 		}
 
-		[HttpGet("{id:int}")]
-		[ProducesResponseType(200)]
-		[ProducesResponseType(400)]
-		[ProducesResponseType(404)]
+
+		//[ProducesResponseType(200)]
+		//[ProducesResponseType(400)]
+		//[ProducesResponseType(404)]
 		//[ProducesResponseType(200, Type = typeof(VillaDto))] 
-		// if you use the IActionResult instead of the ActionResult<>
+		// if you use the IActionResult instead of the ActionResult<> then you may need to specify a typeof
+		[HttpGet("{id:int}", Name = "GetVilla")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
 		public ActionResult<VillaDto> GetVilla(int id)
 		{
 			if(id == 0)
@@ -33,7 +38,34 @@ namespace MagicVillaAPI.Controllers
 			{
 				return NotFound();
 			}
+
 			return Ok(villa);
 		}
+
+		[ProducesResponseType(StatusCodes.Status201Created)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[HttpPost]
+		public ActionResult<VillaDto> CreateVilla([FromBody]VillaDto villaDto)
+		{
+			if(VillaStore.villaDtos.FirstOrDefault(row => row.Name.ToLower() == villaDto.Name.ToLower()) != null)
+			{
+				ModelState.AddModelError("CustomError", "Villa already Exist");
+				return BadRequest(ModelState);
+			}
+			if(villaDto == null)
+			{
+				return BadRequest(villaDto);
+			}
+			else if(villaDto.Id > 0)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError);
+			}
+			villaDto.Id = VillaStore.villaDtos.OrderByDescending(x => x.Id).FirstOrDefault().Id + 1;
+			VillaStore.villaDtos.Add(villaDto);
+			//return Ok(villaDto);
+			return CreatedAtRoute("GetVilla", new { id = villaDto.Id }, villaDto);
+		}
+
 	}
 }
--- MagicVillaAPI/DTOs/VillaDto.cs
+++ MagicVillaAPI/DTOs/VillaDto.cs
@@ -1,8 +1,13 @@
-﻿namespace MagicVillaAPI.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVillaAPI.DTOs
 {
 	public class VillaDto
 	{
 		public int Id { get; set; }
+
+		[Required]
+		[MaxLength(30)]
 		public string Name { get; set; } = string.Empty;
 	}
 }
