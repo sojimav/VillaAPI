389bd9cb617d03013c63e296ab3f0862d28056d3
Victor Ajibade <adesojimav@gmail.com>
2023-07-22 22:34:46 +0100

    updates

--- MagicVillaAPI/Program.cs
+++ MagicVillaAPI/Program.cs
@@ -1,15 +1,24 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers(option => option.ReturnHttpNotAcceptable = true).AddNewtonsoftJson()
+//Using Serilog (helps to addd log information to file)
+
+Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
+	.WriteTo.File("log/villaLogs.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+
+builder.Host.UseSerilog();
+
+
+builder.Services.AddControllers(/*option => option.ReturnHttpNotAcceptable = true*/).AddNewtonsoftJson()
 	.AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
